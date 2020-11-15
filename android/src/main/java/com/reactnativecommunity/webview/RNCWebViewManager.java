1297194d2b109c826c17ed247cf02620416a2f53
Cooper Walter <40807488+cooperwalter@users.noreply.github.com>
2020-11-14 16:59:59 -0800

    Fix (hopefully) issue where videos play in background.

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -1241,12 +1241,12 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
 
     @Override
     public void onHostResume() {
-      // do nothing
+      super.onResume();
     }
 
     @Override
     public void onHostPause() {
-      // do nothing
+      super.onPause();
     }
 
     @Override
